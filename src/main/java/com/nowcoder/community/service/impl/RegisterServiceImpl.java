0c2373cc846f34c05988c3bd167a61c1633f136b
lilstar <2935437378@qq.com>
2023-02-07 17:56:43 +0800

    新增过滤关键字算法

--- src/main/java/com/nowcoder/community/service/impl/RegisterServiceImpl.java
+++ src/main/java/com/nowcoder/community/service/impl/RegisterServiceImpl.java
@@ -8,7 +8,6 @@ import com.nowcoder.community.utils.CommunityUtil;
 import com.nowcoder.community.utils.MailClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
